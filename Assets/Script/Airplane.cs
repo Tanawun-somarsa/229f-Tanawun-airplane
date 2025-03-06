b20ff94679cb177b03632edd884ab9c217c9633c
Tanawun-somarsa <tanawun.soma@bumail.net>
2025-03-06 16:06:31 +0700

    in the air

---
+++ Assets/Script/Airplane.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float eninePower = 20f;
+    [SerializeField] float liftBooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * eninePower);
+        }
+
+        Vector3 lift = Vector3.Project(rb.linearVelocity,transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+    }
+}
